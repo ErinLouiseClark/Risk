2bf70709bb2fd61edab822339e06bdc9b013ba45
erin <erin@192.168.1.18>
2016-09-30 01:22:26 -0600

    Homework 9/29/16 - Need to continue computer move class

--- src/ComputerMove.java
+++ src/ComputerMove.java
@@ -3,9 +3,36 @@ public class ComputerMove {
 	
 	ArrayList<String> computerTerritories = new ArrayList<String>();
 	int counter = 0;
+	int armies = 0;
+	boolean add = true;
 	
 	public void computerMove(){
 		System.out.println("It is the computer's move.");
+		if(Player.players.get(0).getName().getClass().equals(Player.name2)){
+			for(int i = 1; i < 5; i++){
+				if(Player.players.get(0).getContinents() == i){
+					Player.players.get(0).setArmies(i);
+					armies = i;
+					
+				}
+			}
+		}
+		else{
+			for(int i = 1; i < 5; i++){
+				if(Player.players.get(1).getContinents() == i){
+					Player.players.get(1).setArmies(i);
+					armies = i;
+				}
+			}
+		}
+		while(add){
+			int random = (int)(Math.random()*29)+1;
+				if(Territory.spaces.get(random).getPlayerInControl().equals(Player.name2)){
+					Territory.spaces.get(random).setArmiesInTerritory(Territory.spaces.get(random).getArmiesInTerritory()+1);
+					add = false;
+				}
+		}
+		
 		for(int i = 0; i < 30; i++){
 			if(Territory.spaces.get(i).getPlayerInControl().equals(Player.name2)){
 				computerTerritories.add(Territory.spaces.get(i).getNameOfSpace());
